c467be0f4ccc656f89c2ece3265aef5cc91758e2
cooolr <32413353+cooolr@users.noreply.github.com>
2023-02-25 18:27:32 +0800

    add aoto login function

--- chatgpt_proxy_website.py
+++ chatgpt_proxy_website.py
@@ -1,42 +1,54 @@
 # -*- coding: utf-8 -*-
 '''
-第一作者：cooolr
-第二作者：chatgpt
-日期：2023-02-22
+First Author：cooolr
+Second Author：chatgpt
+Date：2023-02-22
 '''
 
 import os
 import requests
 from hashlib import md5
+from auth import get_cookies
 from urllib.parse import unquote
 from flask import Flask, request, redirect, send_file, Response, stream_with_context, make_response
 from werkzeug.routing import BaseConverter
 
-# 使用gevent异步框架运行，flask自带运行太卡了
+# Use gevent to speed up if needed.
+'''
 from gevent.pywsgi import WSGIServer
 from gevent import monkey
 monkey.patch_all()
+'''
 
+# Proxy can be set if needed.
 proxies = {"https": ""}
 
-# 定义user_id 以test@qq.com 123456为例
-# user_id = md5(('test@qq.com123456').encode()).hexdigest()
+# Must and Required parameter.
+_puid = ""
+
+# Password can be added if needed. example:  md5(('test@qq.com123456').encode()).hexdigest()
 user_id = ""
 
-# 定义Cookie参数
-# 需要在cookie获取以下三个参数，_puid为plus会员专属，没它不行
-_puid = ""
-cf_clearance = ""
+# ChatGPT account password login without session_token if needed.
+email_address = ""
+password = ""
+
+# session_token login without chatgpt account if needed.
 session_token = ""
 
-# 请求头
+# Login and get cookie_dict
+cookie_dict = get_cookies(_puid, email_address, password) \
+              if email_address and password \
+              else {"_puid":_puid, "__Secure-next-auth.session-token":session_token}
+cookie = '; '.join([f'{k}={v}' for k,v in cookie_dict.items()])
+
 headers = {
     'authority': 'chat.openai.com',
     'accept': 'text/event-stream',
     'accept-language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
     'cache-control': 'no-cache',
     'content-type': 'application/json',
-    'cookie': f'cf_clearance={cf_clearance}; __Secure-next-auth.session-token={session_token}; _puid={_puid}',
+    'cookie': cookie,
     'dnt': '1',
     'origin': 'https://chat.openai.com',
     'pragma': 'no-cache',
@@ -50,40 +62,37 @@ headers = {
 }
 
 def get_authorization():
-    """获取 accessToken"""
+    """get accessToken"""
     url = "https://chat.openai.com/api/auth/session"
     r = requests.get(url, headers=headers, proxies=proxies)
     authorization = r.json()["accessToken"]
     return "Bearer "+authorization
 
-# 获取 accessToken 并设置到 headers 中
+# set accessToken
 headers["authorization"] = get_authorization()
 
-# 定义 Flask 应用程序
 app = Flask(__name__)
 
-# 自定义正则表达式转换器
+# custom regex converter
 class RegexConverter(BaseConverter):
     def __init__(self, map, *args):
         self.map = map
         self.regex = args[0]
 
-# 注册正则表达式转换器
+# register regex converter
 app.url_map.converters['regex'] = RegexConverter
 
-# 将 cookie 存储到字典中
-cookie_dict = {cookie.split("=")[0]: cookie.split("=")[1] for cookie in headers["cookie"].split("; ")}
-
-# 创建存储资源的目录
+# if change http://127.0.0.1:8011, should be delete.
 resource_dir = './resource'
 os.makedirs(resource_dir, exist_ok=True)
 
-# 定义登录请求和设置cookie
+# define the login request page
 with open('login.html', 'r', encoding='utf-8') as f:
     login_html = f.read()
 with open('login_failed.html', 'r', encoding='utf-8') as f:
     login_failed_html = f.read()
 
+# login authentication
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.cookies.get("accessToken")  == user_id:
@@ -101,45 +110,45 @@ def login():
     else:
         return login_failed_html
 
-# 处理所有的 HTTP 请求方法
+# handles all HTTP request methods
 @app.route('/<path:uri>', methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'TRACE', 'CONNECT', 'PATCH'])
 def index(uri):
-    # 验证cookie
+    # authentication cookie
     if user_id and request.cookies.get("accessToken") != user_id:
         return redirect("/", 302)
-    # 注销
+    # signout
     if 'auth/signout' in uri:
         resp = make_response('{"url":"/"}'.encode())
         resp.delete_cookie("accessToken")
         return resp
     param = '&'.join([f'{i}={j}' for i,j in request.args.items()])
     url = f"https://chat.openai.com/{uri}?{param}" if param else f"https://chat.openai.com/{uri}"
-    # 如果请求的是静态资源，优先从本地获取，否则从远程获取
+    # If the request is a static resource, otherwise get it from the remote
     if any(x in url for x in ('.jpg', '.png', '.ico', '.woff', '.otf', '.css')):
         ext = url.split('.')[-1]
         filename = md5(url.encode('utf-8')).hexdigest()
         filepath = os.path.join(resource_dir, f'{filename}.{ext}')
         if os.path.isfile(filepath):
-            # 如果本地存在该静态资源，则返回该资源
             return send_file(filepath)
         else:
-            # 否则，从远程获取资源并保存到本地，再返回该资源
             r = requests.get(url, headers=headers, cookies=cookie_dict)
             with open(filepath, 'wb') as f:
                 f.write(r.content)
             return send_file(filepath)
     elif 'conversation' in url:
-        # 如果请求的是实时对话，则使用流式处理响应，提高效率
+        # If a live conversation is requested, the response is streamed
         r = requests.request(request.method, url, headers=headers, cookies=cookie_dict, data=request.data, proxies=proxies, stream=True)
         response = Response(stream_with_context(r.iter_content(chunk_size=1024)))
         response.headers['content-type'] = r.headers.get('content-type')
         return response
     else:
-        # 对于其他请求，则使用常规方式处理
+        headers['cookie'] = '; '.join([f'{k}={v}' for k,v in cookie_dict.items()])
         r = requests.request(request.method, url, headers=headers, cookies=cookie_dict, data=request.data, proxies=proxies)
+        cookie_dict.update(r.cookies)
+        # replace http://127.0.0.1:8011 if needed.
         return r.content.replace(b'https://chat.openai.com', b'http://127.0.0.1:8011')
 
 if __name__ == "__main__":
-    # app.run(port=8011, threaded=True)
-    WSGIServer(("127.0.0.1", 8011), app).serve_forever()
-    # 在浏览器打开: http://127.0.0.1:8011/chat
+    app.run(port=8011, threaded=True)
+    # WSGIServer(("127.0.0.1", 8011), app).serve_forever()
+    # open in browser: http://127.0.0.1:8011/chat
